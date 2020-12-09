66b876763e32c74bbe185f98684c0e40a68590c2
Piotr Bara <piotrbara@enigmapattern.com>
2020-12-09 08:35:38 +0100

    Move 2019 challenge to subdirectory
    - Also: rearrange importing Intcode
    
    Signed-off-by: Piotr Bara <piotrbara@enigmapattern.com>

--- day7/7.py
+++ day7/7.py
@@ -1,70 +1,43 @@
-import sys
-from functools import partial
 from pathlib import Path
 
-from tqdm import tqdm
+inp = Path('input.txt').read_text()
+part1, part2 = 0, 0
 
-from .intcode7 import Computer
-import itertools
+want = 'shiny gold'
+rules = {}
 
-debug = partial(print, file=sys.stderr)
-code = [int(d) for d in Path('input7.txt').read_text().split(',')]
+for line in inp.splitlines():
+    rule = line.replace('bags', '').replace('bag', '').split('contain')
+    outer, inners = rule
+    outer = outer.strip()
 
-
-def test_settings(settings_list):
-    for phase_setting in tqdm(settings_list):
-        output_code = -1
-        input_code = 0
-
-        for phase in phase_setting:
-            comp = Computer(code)
-            program = comp.run()
-            next(program)
-            program.send(phase)
-            output_code = program.send(input_code)
-            input_code = output_code
-
-        yield output_code, phase_setting
+    inners = [i.strip().split(' ', 1) for i in inners.strip('. ').split(',')]
+    inners = [(int(n), color) for n, color in inners if n != 'no']
+    rules[outer] = inners
 
 
-print(
-    max(test_settings(itertools.permutations(range(5))), key=lambda x: x[0])
-)
+def all_containers_of(bag):
+    containers = set()
+    for outer, inners in rules.items():
+        if bag in {col for _, col in inners}:
+            containers.add(outer)
+            containers |= all_containers_of(outer)
+
+    return containers
 
 
-def test_settings_feedback(settings_list):
-    for phase_setting in tqdm(settings_list):
-        computers = [Computer(code) for _ in range(5)]
-        out_signal = -1
-        in_signal = 0
-
-        programs = []
-        # initialization, phase + 1st input.txt
-        for comp, phase in zip(computers, phase_setting):
-            program = comp.run()
-
-            next(program)
-            program.send(phase)
-            out_signal = program.send(in_signal)
-            in_signal = out_signal
-
-            programs.append(program)
-
-        i = 5
-        for program in itertools.cycle(programs):
-            try:
-                next(program)
-                out_signal = program.send(in_signal)
-                in_signal = out_signal
-            except StopIteration:
-                break
-            i += 1
-
-        # print(out_signal, phase_setting)
-        yield out_signal, phase_setting, i
+part1 = all_containers_of(want)
+print(len(part1))
 
 
-print(
-    max(test_settings_feedback(itertools.permutations(range(5, 10))), key=lambda x: x[0])
-)
-print('done')
+def bags_within(container) -> int:
+    bags = 0
+    for n, color in rules[container]:
+        bags += n + n * bags_within(color)
+        print(container, '->', n, color)
+
+    return bags
+
+
+part2 = bags_within(want)
+print(part2)
