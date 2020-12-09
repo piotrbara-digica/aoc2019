6f071cb3de500ea74b84f900e1e5aeab42016996
Piotr Bara <piotrbara@enigmapattern.com>
2020-12-09 08:36:05 +0100

    2020 Day 9 solution and input
    Signed-off-by: Piotr Bara <piotrbara@enigmapattern.com>

---
+++ day9/9.py
@@ -0,0 +1,28 @@
+from itertools import *
+from pathlib import Path
+
+inp = Path('input.txt').read_text().splitlines()
+inp = [int(i) for i in inp]
+
+
+def part1(code, plen=25):
+    for i, num in enumerate(code[plen:]):
+        if not any(num == sum(pair)
+                   for pair in combinations(code[i: i + plen], 2)
+                   if pair[0] != pair[1]):
+            return num
+
+
+def part2(code, want):
+    code.remove(want)
+    for r in range(2, 50):
+        for i in range(len(code) - r):
+            cont = code[i:i + r]
+            if sum(cont) == want:
+                return min(cont) + max(cont)
+
+
+p1 = part1(inp, 25)
+print(p1)
+p2 = part2(inp, p1)
+print(p2)
